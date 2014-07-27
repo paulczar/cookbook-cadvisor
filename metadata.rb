a80dedbd2eb25f4adcaaf7e370a0288d77eff8f7
paulczar <paul@paulcz.net>
2014-07-27 14:41:01 -0500

    first commit

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+# Encoding: utf-8
+name             'cadvisor'
+maintainer       'Paul Czarkowski'
+maintainer_email 'username.taken@gmail.com'
+license          'Apache2'
+description      'Installs/Configures cadvisor'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
+
+%w(docker).each do |ckbk|
+  depends ckbk
+end
